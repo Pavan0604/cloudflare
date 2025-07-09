6f4bf243bae92708b3efab10d4051f5ab7b19b9e
pavan.kumar <pavan.kumar@clevertap.com>
2025-07-09 13:35:22 +0530

    Refactor get_region.py to improve argument handling and condition checks

--- scripts/get_region.py
+++ scripts/get_region.py
@@ -1,19 +1,5 @@
-import boto3
 import sys
 import re
-
-if len(sys.argv) < 2:
-    print("❌ Usage: python trigger_ssm.py '<description>'")
-    sys.exit(1)
-
-description = sys.argv[1]
-print(f"📄 Description: {description}")
-
-
-targeting_trigger = "Too few LC cache Redis write operations for TARGETING"
-if targeting_trigger not in description:
-    print("✅ Alert does not match TARGETING condition. Skipping.")
-    sys.exit(0)
 
 region_map = {
     "EU": "eu-west-1",
@@ -37,14 +23,21 @@ def get_region_from_description(description):
         return region_map["SG"]
     elif "SK" in description:
         return region_map["SK"]
-    elif "aps3" in description:
+    elif "APS3" in description:
         return region_map["aps3"]
-    elif "i1" in description:
+    elif "I1" in description:
         return region_map["i1"]
-    elif "mec1" in description:
+    elif "MEC1" in description:
         return region_map["mec1"]
     else:
         print("❌ No matching region found in description.")
         sys.exit(1)
 
-region = get_region_from_description(description)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("❌ Usage: python trigger_ssm.py '<description>'")
+        sys.exit(1)
+
+    description = sys.argv[1]
+    region = get_region_from_description(description)
+    print(region)
